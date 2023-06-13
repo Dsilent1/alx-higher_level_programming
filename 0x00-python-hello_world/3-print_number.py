f1984bd7a209f15a40dc06f2ceb2d32bbe339721
Dsilent1 <emmanuelalaibe7@gmail.com>
2023-06-13 23:50:51 +0000

    4-print_float.py

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-"""it prints 3-print_number.py"""
-number = 3.14159
-print(f"Float: {number:.2f}")
+"""it prints"""
+number = 98
+print(f"{number:d} Battery street")
---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""it prints"""
+number = 3.14159
+print(f"Float: {number:.2f}")
