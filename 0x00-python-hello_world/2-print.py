aff64234aa26218d1ae179b0486cc4b4719b7a61
Dsilent1 <emmanuelalaibe7@gmail.com>
2023-06-13 23:43:12 +0000

    task 2 and 3

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+"""it will print Programming is like building a multilingual puzzle"""
+print('"Programming is like building a multilingual puzzle')
---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""it prints 3-print_number.py"""
+number = 3.14159
+print(f"Float: {number:.2f}")
